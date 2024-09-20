52fc15145a682c86bd1ee01dfe7609222ba444ac
andruchristo27 <e41211364@student.polije.ac.id>
2024-09-20 09:40:11 +0700

    Book: Create

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BookController;
 
 Route::get('/books', [BookController::class, 'index']);
+Route::post('/books', [BookController::class, 'store']);
