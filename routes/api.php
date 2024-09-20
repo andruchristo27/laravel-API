09abfb70fc9385db9698fb8b107af70f83167fea
andruchristo27 <e41211364@student.polije.ac.id>
2024-09-20 09:47:50 +0700

    Api Resource

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BookController;
 
-Route::get('/books', [BookController::class, 'index']);
-Route::post('/books', [BookController::class, 'store']);
-Route::get('/books/{book}', [BookController::class, 'show']);
-Route::put('/books/{book}', [BookController::class, 'update']);
-Route::delete('/books/{book}', [BookController::class, 'destroy']);
+Route::apiResource('books', BookController::class);
