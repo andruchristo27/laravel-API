07af205370729fef958751b4428c00e0c9749742
andruchristo27 <e41211364@student.polije.ac.id>
2024-09-20 09:51:43 +0700

    Authentication: Register

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\AuthenticationController;
 
 Route::apiResource('books', BookController::class);
+Route::post('register', [AuthenticationController::class, 'register']);
