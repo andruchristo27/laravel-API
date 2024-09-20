c893780b5f930f80704a31b893e318a2c0131a4a
andruchristo27 <e41211364@student.polije.ac.id>
2024-09-20 09:55:42 +0700

    Authentication: Me

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,4 @@ use App\Http\Controllers\Api\AuthenticationController;
 Route::apiResource('books', BookController::class);
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
+Route::get('me', [AuthenticationController::class, 'me'])->middleware('auth:sanctum');
